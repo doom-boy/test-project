7406bf3c8562917c262859ff9fec64b5e4b0019c
elijah <eechan@cpp.edu>
2024-09-01 01:32:40 -0700

    logs

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld{
     public static void main(String[] args){
         String message = "Hello World";
         System.out.print(message);
+        System.out.print(message);
 
     }
 }
